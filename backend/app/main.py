a3bd2696f348eefb49d30533af83c72d95ec1b4f
ApekshaPai18 <91828694+ApekshaPai18@users.noreply.github.com>
2026-07-13 20:07:36 +0530

    Update CORS middleware to allow all origins
    Comment out specific allowed origins for CORS configuration.

--- backend/app/main.py
+++ backend/app/main.py
@@ -31,11 +31,11 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-    "http://localhost:3000",
-    "https://emotion-aware-intelligent-tutoring.vercel.app",  # ✅ Your main domain
-    "https://emotion-aware-intelligent-tutoring-g01t4g686.vercel.app",  # ✅ Your backup domain
-],  # React dev server
+#     allow_origins=[
+#     "http://localhost:3000",
+#     "https://emotion-aware-intelligent-tutoring.vercel.app",  # ✅ Your main domain
+#     "https://emotion-aware-intelligent-tutoring-g01t4g686.vercel.app",  # ✅ Your backup domain
+# ],  # React dev server
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
